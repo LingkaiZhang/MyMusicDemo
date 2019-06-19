48a0d521dff71fa71fb167db78533c56e6488b6b
lingkai <lingkai2313@163.com>
2019-06-19 14:16:59 +0800

    init commit

---
+++ app/src/main/java/com/lingkai/mymusic/util/Consts.java
@@ -0,0 +1,8 @@
+package com.lingkai.mymusic.util;
+
+import com.lingkai.mymusic.BuildConfig;
+
+public class Consts {
+    public static final String RESOURCE_PREFIX = BuildConfig.RESOURCE_PREFIX;
+    public static final String ENDPOINT = BuildConfig.ENDPOINT;
+}
