a30bb6bdc71286efeefebc0012e7e7847a855dac
lingkai <lingkai2313@163.com>
2019-06-20 16:11:35 +0800

    登录注册

--- app/src/main/java/com/lingkai/mymusic/activity/LoginActivity.java
+++ app/src/main/java/com/lingkai/mymusic/activity/LoginActivity.java
@@ -1,15 +1,62 @@
 package com.lingkai.mymusic.activity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.lingkai.mymusic.MainActivity;
 import com.lingkai.mymusic.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.OnClick;
+
+public class LoginActivity extends BaseCommonActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        //21
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+            //状态栏颜色设置为透明
+            Window window = getWindow();
+            window.setStatusBarColor(Color.TRANSPARENT);
+
+            //去除半透明状态栏(如果有)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            //SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN：让内容显示到状态栏
+            //SYSTEM_UI_FLAG_LAYOUT_STABLE：状态栏文字显示白色
+            //SYSTEM_UI_FLAG_LIGHT_STATUS_BAR：状态栏文字显示黑色
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+    }
+
+    @OnClick(R.id.bt_login)
+    public void bt_login() {
+        startActivity(LoginPhoneActivity.class);
+    }
+
+
+    @OnClick(R.id.bt_register)
+    public void bt_register() {
+        startActivity(RegisterActivity.class);
+    }
+
+
+    @OnClick(R.id.tv_enter)
+    public void tv_enter() {
+        startActivity(MainActivity.class);
     }
 }
---
+++ app/src/main/java/com/lingkai/mymusic/activity/LoginPhoneActivity.java
@@ -0,0 +1,15 @@
+package com.lingkai.mymusic.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lingkai.mymusic.R;
+
+public class LoginPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_phone);
+    }
+}
