4e4efb2f27ff349fb34ab1f6551bcb5db9ac0afc
lingkai <lingkai2313@163.com>
2019-06-20 20:33:51 +0800

    登录页面完成。

--- app/src/main/java/com/lingkai/mymusic/activity/LoginPhoneActivity.java
+++ app/src/main/java/com/lingkai/mymusic/activity/LoginPhoneActivity.java
@@ -2,14 +2,100 @@ package com.lingkai.mymusic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.lingkai.mymusic.MainActivity;
 import com.lingkai.mymusic.R;
+import com.lingkai.mymusic.api.Api;
+import com.lingkai.mymusic.domain.Session;
+import com.lingkai.mymusic.domain.User;
+import com.lingkai.mymusic.domain.event.LoginSuccessEvent;
+import com.lingkai.mymusic.domain.response.DetailResponse;
+import com.lingkai.mymusic.reactivex.HttpListener;
+import com.lingkai.mymusic.util.StringUtil;
+import com.lingkai.mymusic.util.ToastUtil;
 
-public class LoginPhoneActivity extends AppCompatActivity {
+import org.apache.commons.lang3.StringUtils;
+import org.greenrobot.eventbus.EventBus;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class LoginPhoneActivity extends BaseTitleActivity {
+
+    @BindView(R.id.et_phone)
+    EditText et_phone;
+
+    @BindView(R.id.et_password)
+    EditText et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_phone);
     }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        enableBackMenu();
+    }
+
+    @OnClick(R.id.bt_login)
+    public void bt_login() {
+        String phone = et_phone.getText().toString();
+        if (StringUtils.isBlank(phone)) {
+            ToastUtil.showSortToast(getActivity(), R.string.hint_phone);
+            return;
+        }
+
+        if (!StringUtil.isPhone(phone)) {
+            ToastUtil.showSortToast(getActivity(), R.string.hint_error_phone);
+            return;
+        }
+
+        String password = et_password.getText().toString();
+        if (StringUtils.isBlank(password)) {
+            ToastUtil.showSortToast(getActivity(), R.string.hint_password);
+            return;
+        }
+
+        if (!StringUtil.isPassword(password)) {
+            ToastUtil.showSortToast(getActivity(), R.string.hint_error_password_format);
+            return;
+        }
+
+        User user = new User();
+        user.setPhone(phone);
+        user.setPassword(password);
+        user.setType(User.TYPE_PHONE);
+
+        Api.getInstance().login(user)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new HttpListener<DetailResponse<Session>>(getActivity()) {
+                    @Override
+                    public void onSucceeded(DetailResponse<Session> data) {
+                        super.onSucceeded(data);
+                        next(data.getData());
+                    }
+                });
+    }
+
+    /**
+     * 登陆完成后，保存相关信息，并跳转到主界面
+     *
+     * @param data
+     */
+    private void next(Session data) {
+        sp.setToken(data.getToken());
+        sp.setUserId(data.getId());
+        sp.setIMToken(data.getIm_token());
+        startActivityAfterFinishThis(MainActivity.class);
+
+        EventBus.getDefault().post(new LoginSuccessEvent());
+    }
+
 }
