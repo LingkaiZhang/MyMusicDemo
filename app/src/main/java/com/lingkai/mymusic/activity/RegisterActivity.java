7946ab2ca37c2d52e52c7a5469444e7af1763576
lingkai <lingkai2313@163.com>
2019-06-20 16:34:27 +0800

    注册页面样式完善

--- app/src/main/java/com/lingkai/mymusic/activity/RegisterActivity.java
+++ app/src/main/java/com/lingkai/mymusic/activity/RegisterActivity.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 
 import com.lingkai.mymusic.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BaseTitleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        enableBackMenu();
     }
 }
