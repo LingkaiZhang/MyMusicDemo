dcaab3b9dced841559aad4c4fd4b0d8a122227f9
lingkai <lingkai2313@163.com>
2019-07-01 17:46:43 +0800

    播放界面自定义指针和黑胶唱片View.

--- app/src/main/java/com/lingkai/mymusic/view/RecordBackgroundView.java
+++ app/src/main/java/com/lingkai/mymusic/view/RecordBackgroundView.java
@@ -1,29 +1,89 @@
 package com.lingkai.mymusic.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.lingkai.mymusic.R;
+import com.lingkai.mymusic.util.DensityUtil;
+
 /**
- * description ： TODO:类的作用
- * author : lingkai
- * date : 2019/6/27 20:31
+ * Created by smile on 2018/6/22.
  */
+
 public class RecordBackgroundView extends View {
+    /**
+     * Cd白圈背景的比例
+     */
+    public static final float CD_BG_SCALE = 1.3F;
+
+    /**
+     * 白圈
+     */
+    private Drawable cdBg;
+
+    /**
+     * CD白圈背景到顶部的比例
+     */
+    public static final float CD_BG_TOP_SCALE = 17.052F;
+
     public RecordBackgroundView(Context context) {
         super(context);
+        init();
     }
 
     public RecordBackgroundView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public RecordBackgroundView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
-    public RecordBackgroundView(Context context,  @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public RecordBackgroundView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
     }
+
+    private void init() {
+        cdBg = getResources().getDrawable(R.drawable.shape_cd_bg);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        int widthHalf = measuredWidth / 2;
+
+        int cdBgWidth = (int) (measuredWidth / CD_BG_SCALE);
+        int cdBgWidthHalf = cdBgWidth / 2;
+
+        //cd背景
+        int cdBgLeft = widthHalf - cdBgWidthHalf;
+        int cdBgTop = DensityUtil.dip2px(getContext(), measuredWidth / CD_BG_TOP_SCALE);
+        cdBg.setBounds(cdBgLeft, cdBgTop, cdBgLeft + cdBgWidth, cdBgTop + cdBgWidth);
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.save();
+
+        //绘制背景
+        cdBg.draw(canvas);
+
+        canvas.restore();
+    }
+
 }
